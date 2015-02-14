c8710f014058d0f9fca57c39e68354878d75b15c
John Pizzo <jjpizzo4@gmail.com>
2015-02-14 16:50:22 -0500

    Fixed reject and uniq

--- src/underbar.js
+++ src/underbar.js
@@ -87,7 +87,7 @@
 
     for(var i = 0; i < collection.length; i++)
     {
-      if(test(collection[i]))
+      if(test(collection[i], collection))
         newCollection.push(collection[i]);
     }
     return newCollection;
@@ -97,10 +97,20 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function (val){return !test(val);});
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var newCollection = [];
+
+    for(var i = 0; i < array.length; i++)
+    {
+      if(_.indexOf(newCollection, array[i]) === -1)
+        newCollection.push(array[i]);
+
+    }
+    return newCollection;
   };
 
 
